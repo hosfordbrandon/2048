1e08011f47771583e4a341640da5d03a7a76db0f
hosfordryan <hosfordryan@gmail.com>
2016-01-14 08:57:54 -0600

    Tiles and Board

--- 2048/Assets/Scripts/BoardManager.cs
+++ 2048/Assets/Scripts/BoardManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class test : MonoBehaviour {
+public class BoardManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
