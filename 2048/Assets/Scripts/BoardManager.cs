77bba0af4ad23b3a3528f7d95931ca17d4ba43c1
hosfordryan <hosfordryan@gmail.com>
2016-01-14 09:46:38 -0600

    Thnigs

--- 2048/Assets/Scripts/BoardManager.cs
+++ 2048/Assets/Scripts/BoardManager.cs
@@ -3,11 +3,25 @@ using System.Collections;
 
 public class BoardManager : MonoBehaviour {
 
-	// Use this for initialization
+	public int[][] board= new int[4][4];
+
+
+
 	void Start () {
-	
+		int startX = UnityEngine.Random.Range(0,5);
+
+
+		for(int i =0;i<board.Length;i++){
+			for(int j =0; j<board[i].Length;j++){
+				board[i][j] = 0;
+			}
+		}
+
+
 	}
 	
+
+
 	// Update is called once per frame
 	void Update () {
 	
