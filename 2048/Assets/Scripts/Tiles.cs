4f21c2ae11d6c4fbec736b57e8ffc79a748b26db
hosfordryan <hosfordryan@gmail.com>
2016-01-15 09:20:33 -0600

    Raycast starting

---
+++ 2048/Assets/Scripts/Tiles.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tiles : MonoBehaviour {
+
+	public int value;
+	public float speed = 60;
+
+	private Rigidbody2D rBody;
+	private BoxCollider2D GOcollider;
+	private bool notCD = true;
+	// Use this for initialization
+	void Start () {
+		rBody = gameObject.GetComponent<Rigidbody2D>();
+		GOcollider = gameObject.GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if(Input.GetKey(KeyCode.UpArrow)){
+			GOcollider.enabled = false;
+			RaycastHit2D rayHit = Physics2D.Raycast(transform.position+new Vector3(0.5f,0,0),Vector2.up);
+			Debug.DrawRay(transform.position+new Vector3(0.5f,0,0), Vector2.up,Color.red);
+			GOcollider.enabled = true;
+			Debug.Log(rayHit.collider.gameObject.name);
+		}
+
+
+	}
+
+
+}
